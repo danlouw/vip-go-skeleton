ee87538aa43010a2bad0ca0f34bfedb87e9714ef
Simon Wheatley <simonw@automattic.com>
2018-03-15 21:28:13 +0000

    Build changes from 0a407518d1c8948dfce4c3f6998bf448fdac5aa0
    https://circleci.com/gh/Automattic/vip-go-skeleton/23

---
+++ vip-config/vip-config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Hi there, VIP dev!
+ *
+ * vip-config.php is where you put things you'd usually put in wp-config.php. Don't worry about database settings
+ * and such, we've taken care of that for you. This is just for if you need to define an API key or something
+ * of that nature.
+ *
+ * Happy Coding!
+ *
+ * - The WordPress.com VIP Team
+ **/
+
+// Just a change 1
+// Just another change 2
+// Further change
+// Change is the only constant
+// Just filling up your GitHub feeds like 😎
